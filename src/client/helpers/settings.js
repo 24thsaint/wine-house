4b76492f345b0b9ba1de01c9461dbdb8251ae610
24thsaint <admin@blackout.biz.tm>
2018-06-19 07:50:27 +0800

    fix(Settings): Pagination handled for data patching

--- src/client/helpers/settings.js
+++ src/client/helpers/settings.js
@@ -16,8 +16,8 @@ class Setting {
   async set(key, value) {
     let setting;
     const existingSetting = await this.settingService.find({ key });
-    if (existingSetting.length > 0) {
-      setting = await this.settingService.patch(existingSetting[0]._id, {value});
+    if (existingSetting.total > 0) {
+      setting = await this.settingService.patch(existingSetting.data[0]._id, {value});
     } else {
       setting = await this.settingService.create({key, value});
     }
