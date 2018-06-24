15442c70757e406ccbeeba71d6960350c284d1ea
24thsaint <admin@blackout.biz.tm>
2018-06-25 02:41:33 +0800

    fix(Heroku): Deploy 2

--- src/hooks/onlyMaster.js
+++ src/hooks/onlyMaster.js
@@ -1,4 +1,10 @@
 module.exports = async function execute(context) {
+  const response = await context.app.service('/api/users').find();
+
+  if (response.total <= 0) {
+    return context;
+  }
+  
   if (context.params.user.status === 'master') {
     return context;
   } else {
